70662f5639bce09606603ecb04da7e5c6c748f37
Edward Ramírez <edal_ramirez@hotmail.com>
2021-08-26 19:39:31 -0500

    Improving modal rendering by checking isActive prop

--- frontend/src/components/UI/Modal/Modal.js
+++ frontend/src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import Auxiliary from '../../../hoc/Auxiliary';
-import Backdrop from '../Backdrop/Backdrop';
-import './Modal.css'
+import React, { Component } from "react";
+import Auxiliary from "../../../hoc/Auxiliary";
+import Backdrop from "../Backdrop/Backdrop";
+import "./Modal.css";
 
-const Modal = (props) => {
-  return (
-    <Auxiliary >
-      <Backdrop isPurchsing={props.isActive} clicked={props.handleModalClicked} />
-      <div
-        className="Modal"
-        style={{
-          transform: props.isActive ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.isActive ? '1' : '0',
-        }}
-      >
-        {props.children}
-      </div>
-    </Auxiliary>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.isActive !== this.props.isActive;
+  }
+
+  render() {
+    return (
+      <Auxiliary>
+        <Backdrop
+          show={this.props.isActive}
+          clicked={this.props.handleModalClicked}
+        />
+        <div
+          className="Modal"
+          style={{
+            transform: this.props.isActive
+              ? "translateY(0)"
+              : "translateY(-100vh)",
+            opacity: this.props.isActive ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Auxiliary>
+    );
+  }
+}
+
 export default Modal;
